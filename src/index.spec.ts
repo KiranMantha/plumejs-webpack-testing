fc279bf8e05a06ad8275f071d78c76850c570d6f
KiranMantha <kiranv.mantha@gmail.com>
2023-06-29 16:46:39 +0530

    updated unit test for dom testing using @testing-library/dom library

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,19 +1,33 @@
-import { Fixture, TestBed } from '@plumejs/testing';
+import { Fixture, TestBed, flushMicroTasks } from '@plumejs/testing';
+import { getByText, waitFor } from '@testing-library/dom';
+import { vi } from 'vitest';
 import { AppComponent } from './index';
 
 describe('@plumejs/core Component', () => {
   let appRoot: Fixture<AppComponent>;
 
-  beforeAll(async () => {
+  it('translation should return "Welcome to PlumeJS"', async () => {
+    vi.useFakeTimers();
     appRoot = await TestBed.MockComponent(AppComponent);
-  });
-
-  it('translation should return "Welcome to PlumeJS"', () => {
-    const h1 = appRoot.element.querySelector('h1');
+    const { componentInstance, element } = appRoot;
+    const h1 = element.querySelector('h1');
     expect(h1.innerHTML).toBe('Welcome to PlumeJS');
+    // testing component internals
+    // expect(componentInstance.title).toBe('');
+    // vi.advanceTimersByTime(2000);
+    // expect(componentInstance.title).toBe('Hello world');
+    // console.log('title', componentInstance.title);
+    // console.log(element.innerHTML);
+
+    // testing dom changes
+    expect(getByText(element, 'Loading')).toBeInTheDocument();
+    vi.advanceTimersByTime(2000);
+    expect(componentInstance.title).toBe('Hello world');
+    await waitFor(flushMicroTasks());
+    expect(getByText(element, componentInstance.title)).toBeInTheDocument();
   });
 
-  afterAll(() => {
+  afterEach(() => {
     TestBed.RemoveComponent(appRoot);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ import globalstyles from './styles/styles.scss?inline';
   root: true
 })
 export class AppComponent {
+  title = '';
+  mount() {
+    setTimeout(() => {
+      this.title = 'Hello world';
+    }, 2000);
+  }
+
   render() {
     return html`
       <main class="center">
@@ -16,6 +23,7 @@ export class AppComponent {
           Please check
           <a href="https://github.com/KiranMantha/plumejs">here</a> for documentation
         </p>
+        ${this.title ? `<div>${this.title}</div>` : `<div title='loader'>Loading</div>`}
       </main>
     `;
   }
